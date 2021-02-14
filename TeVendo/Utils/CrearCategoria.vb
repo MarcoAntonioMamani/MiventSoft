72713436644108349561910db70ccaa497a75cd0
Marco Mamani <marcoantoniomamanichura@gmail.com>
2021-02-14 16:33:31 -0400

    Programa De Reportes

---
+++ TeVendo/Utils/CrearCategoria.vb
@@ -0,0 +1,59 @@
+﻿
+Imports Janus.Windows.GridEX
+Imports Negocio.AccesoLogica
+Imports DevComponents.DotNetBar
+Public Class CrearCategoria
+    Public IdCategoria As Integer = -1
+    Public Bandera As Boolean = False
+    Dim img As Bitmap = New Bitmap(My.Resources.mensaje, 50, 50)
+    Public Modulo As Integer = 0
+    Private Sub CrearLibreria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbdescripcion.Clear()
+        tbdescripcion.Focus()
+        _habilitarFocus()
+    End Sub
+
+    Public Sub _habilitarFocus()
+        With Highlighter2
+            .SetHighlightOnFocus(tbdescripcion, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(btnGuardar, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(btnSalir, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+
+        End With
+    End Sub
+
+    Private Sub ButtonX1_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If (tbdescripcion.Text.Length > 1) Then
+            Dim id As String = ""
+            Bandera = L_prCategoriaInsertar(id, tbdescripcion.Text, tbdescripcion.Text, 1, "", 1, 1)
+            If (Bandera = True) Then
+
+                IdCategoria = CInt(id.Trim)
+                Me.Close()
+            Else
+                ToastNotification.Show(Me, "Error Al guardar La Categoria", img, 8000, eToastGlowColor.Red, eToastPosition.TopCenter)
+                tbdescripcion.Focus()
+            End If
+
+
+        Else
+            ToastNotification.Show(Me, "Debe Ingresar un Texto Valido en la Descripción", img, 8000, eToastGlowColor.Red, eToastPosition.TopCenter)
+            tbdescripcion.Focus()
+            Return
+
+
+        End If
+    End Sub
+
+    Private Sub tbdescripcion_KeyDown(sender As Object, e As KeyEventArgs) Handles tbdescripcion.KeyDown
+        If (e.KeyData = Keys.Enter) Then
+            btnGuardar.Focus()
+
+        End If
+    End Sub
+
+    Private Sub ButtonX2_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Bandera = False
+        Me.Close()
+    End Sub
+End Class
---
+++ TeVendo/Utils/CrearProveedor.vb
@@ -0,0 +1,57 @@
+﻿Imports Janus.Windows.GridEX
+Imports Negocio.AccesoLogica
+Imports DevComponents.DotNetBar
+Public Class CrearProveedor
+    Public ProveedorId As Integer = -1
+    Public Bandera As Boolean = False
+    Dim img As Bitmap = New Bitmap(My.Resources.mensaje, 50, 50)
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Bandera = False
+        Me.Close()
+    End Sub
+
+    Public Sub _habilitarFocus()
+        With Highlighter2
+            .SetHighlightOnFocus(tbNombreProveedor, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(tbDireccion, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(tbTelefono01, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(tbTelefono02, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(tbNroDocumento, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(cbTipoDocumento, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(tbNombreProveedor, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(btnGuardar, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+            .SetHighlightOnFocus(btnSalir, DevComponents.DotNetBar.Validator.eHighlightColor.Blue)
+
+        End With
+    End Sub
+    Private Sub CrearLibreria_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        P_Global._prCargarComboGenerico(cbTipoDocumento, L_prLibreriaDetalleGeneral(8), "cnnum", "Codigo", "cndesc1", "TipoDocumento")
+        tbNombreProveedor.Clear()
+        tbNombreProveedor.Focus()
+        _habilitarFocus()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        If (tbNombreProveedor.Text.Length > 1) Then
+            Dim id As String = ""
+            Bandera = InsertarProveedor(id, tbNombreProveedor.Text, tbDireccion.Text, tbTelefono01.Text, tbTelefono02.Text,
+                                    "", cbTipoDocumento.Value, tbNroDocumento.Text, 1)
+            If (Bandera = True) Then
+
+                ProveedorId = CInt(id.Trim)
+                Me.Close()
+            Else
+                ToastNotification.Show(Me, "Error Al guardar Proveedor", img, 8000, eToastGlowColor.Red, eToastPosition.TopCenter)
+                tbNombreProveedor.Focus()
+            End If
+
+
+        Else
+            ToastNotification.Show(Me, "Debe Ingresar un Nombre Valido del Proveedor", img, 8000, eToastGlowColor.Red, eToastPosition.TopCenter)
+            tbNombreProveedor.Focus()
+            Return
+
+
+        End If
+    End Sub
+End Class
