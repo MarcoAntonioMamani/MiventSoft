036d007b10ee118df6148b210e480a64c7a87129
Marco Mamani <marcoantoniomamanichura@gmail.com>
2021-03-12 05:15:51 -0400

    Formulario Ingreso o Egreso

---
+++ TeVendo/Caja/Tec_ReporteIngresosEgresosMensuales.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Tec_ReporteIngresosEgresosMensuales
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PanelSuperior = New System.Windows.Forms.Panel()
+        Me.PanelLEft = New System.Windows.Forms.Panel()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.PanelDatos = New System.Windows.Forms.Panel()
+        Me.PanelPrincipal = New System.Windows.Forms.Panel()
+        Me.MReportViewer = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.Panel10 = New System.Windows.Forms.Panel()
+        Me.Panel11 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel12 = New System.Windows.Forms.Panel()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PanelButton = New System.Windows.Forms.Panel()
+        Me.PanelNavegacion = New System.Windows.Forms.Panel()
+        Me.PanelToolBar1 = New System.Windows.Forms.Panel()
+        Me.ButtonX1 = New DevComponents.DotNetBar.ButtonX()
+        Me.Panel1.SuspendLayout()
+        Me.PanelSuperior.SuspendLayout()
+        Me.PanelLEft.SuspendLayout()
+        Me.Panel8.SuspendLayout()
+        Me.PanelDatos.SuspendLayout()
+        Me.PanelPrincipal.SuspendLayout()
+        Me.Panel10.SuspendLayout()
+        Me.Panel11.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelButton.SuspendLayout()
+        Me.PanelToolBar1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.PanelSuperior)
+        Me.Panel1.Controls.Add(Me.PanelButton)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1006, 622)
+        Me.Panel1.TabIndex = 8
+        '
+        'PanelSuperior
+        '
+        Me.PanelSuperior.Controls.Add(Me.PanelLEft)
+        Me.PanelSuperior.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelSuperior.Location = New System.Drawing.Point(0, 0)
+        Me.PanelSuperior.Name = "PanelSuperior"
+        Me.PanelSuperior.Size = New System.Drawing.Size(1006, 539)
+        Me.PanelSuperior.TabIndex = 1
+        '
+        'PanelLEft
+        '
+        Me.PanelLEft.Controls.Add(Me.Panel8)
+        Me.PanelLEft.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelLEft.Location = New System.Drawing.Point(0, 0)
+        Me.PanelLEft.Name = "PanelLEft"
+        Me.PanelLEft.Size = New System.Drawing.Size(1006, 539)
+        Me.PanelLEft.TabIndex = 0
+        '
+        'Panel8
+        '
+        Me.Panel8.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(116, Byte), Integer), CType(CType(170, Byte), Integer))
+        Me.Panel8.Controls.Add(Me.PanelDatos)
+        Me.Panel8.Controls.Add(Me.Panel10)
+        Me.Panel8.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel8.Location = New System.Drawing.Point(0, 0)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Padding = New System.Windows.Forms.Padding(3)
+        Me.Panel8.Size = New System.Drawing.Size(1006, 539)
+        Me.Panel8.TabIndex = 1
+        '
+        'PanelDatos
+        '
+        Me.PanelDatos.BackColor = System.Drawing.Color.White
+        Me.PanelDatos.Controls.Add(Me.PanelPrincipal)
+        Me.PanelDatos.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelDatos.Location = New System.Drawing.Point(3, 30)
+        Me.PanelDatos.Name = "PanelDatos"
+        Me.PanelDatos.Size = New System.Drawing.Size(1000, 506)
+        Me.PanelDatos.TabIndex = 2
+        '
+        'PanelPrincipal
+        '
+        Me.PanelPrincipal.BackColor = System.Drawing.SystemColors.Control
+        Me.PanelPrincipal.Controls.Add(Me.MReportViewer)
+        Me.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.PanelPrincipal.Margin = New System.Windows.Forms.Padding(4)
+        Me.PanelPrincipal.Name = "PanelPrincipal"
+        Me.PanelPrincipal.Size = New System.Drawing.Size(1000, 506)
+        Me.PanelPrincipal.TabIndex = 2
+        '
+        'MReportViewer
+        '
+        Me.MReportViewer.ActiveViewIndex = -1
+        Me.MReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.MReportViewer.Cursor = System.Windows.Forms.Cursors.Default
+        Me.MReportViewer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MReportViewer.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MReportViewer.Location = New System.Drawing.Point(0, 0)
+        Me.MReportViewer.Margin = New System.Windows.Forms.Padding(4)
+        Me.MReportViewer.Name = "MReportViewer"
+        Me.MReportViewer.Size = New System.Drawing.Size(1000, 506)
+        Me.MReportViewer.TabIndex = 20
+        Me.MReportViewer.ToolPanelWidth = 267
+        '
+        'Panel10
+        '
+        Me.Panel10.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Panel10.Controls.Add(Me.Panel11)
+        Me.Panel10.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel10.Location = New System.Drawing.Point(3, 3)
+        Me.Panel10.Name = "Panel10"
+        Me.Panel10.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel10.Size = New System.Drawing.Size(1000, 27)
+        Me.Panel10.TabIndex = 1
+        '
+        'Panel11
+        '
+        Me.Panel11.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Panel11.Controls.Add(Me.Label3)
+        Me.Panel11.Controls.Add(Me.Panel12)
+        Me.Panel11.Controls.Add(Me.PictureBox3)
+        Me.Panel11.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel11.Location = New System.Drawing.Point(1, 1)
+        Me.Panel11.Name = "Panel11"
+        Me.Panel11.Size = New System.Drawing.Size(998, 25)
+        Me.Panel11.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(59, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Padding = New System.Windows.Forms.Padding(20, 0, 0, 0)
+        Me.Label3.Size = New System.Drawing.Size(458, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Reporte de Ingresos / Gastos Mensuales"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Panel12
+        '
+        Me.Panel12.BackColor = System.Drawing.Color.White
+        Me.Panel12.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel12.Location = New System.Drawing.Point(58, 0)
+        Me.Panel12.Name = "Panel12"
+        Me.Panel12.Size = New System.Drawing.Size(1, 25)
+        Me.Panel12.TabIndex = 1
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.PictureBox3.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox3.Image = Global.TeVendo.My.Resources.Resources.tec_triangulo_blanco
+        Me.PictureBox3.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Padding = New System.Windows.Forms.Padding(15, 5, 15, 5)
+        Me.PictureBox3.Size = New System.Drawing.Size(58, 25)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 0
+        Me.PictureBox3.TabStop = False
+        '
+        'PanelButton
+        '
+        Me.PanelButton.AutoScroll = True
+        Me.PanelButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.PanelButton.Controls.Add(Me.PanelNavegacion)
+        Me.PanelButton.Controls.Add(Me.PanelToolBar1)
+        Me.PanelButton.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelButton.Location = New System.Drawing.Point(0, 539)
+        Me.PanelButton.Name = "PanelButton"
+        Me.PanelButton.Size = New System.Drawing.Size(1006, 83)
+        Me.PanelButton.TabIndex = 3
+        '
+        'PanelNavegacion
+        '
+        Me.PanelNavegacion.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelNavegacion.Location = New System.Drawing.Point(0, 0)
+        Me.PanelNavegacion.Margin = New System.Windows.Forms.Padding(4)
+        Me.PanelNavegacion.Name = "PanelNavegacion"
+        Me.PanelNavegacion.Size = New System.Drawing.Size(861, 83)
+        Me.PanelNavegacion.TabIndex = 21
+        '
+        'PanelToolBar1
+        '
+        Me.PanelToolBar1.AutoSize = True
+        Me.PanelToolBar1.BackColor = System.Drawing.Color.White
+        Me.PanelToolBar1.Controls.Add(Me.ButtonX1)
+        Me.PanelToolBar1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.PanelToolBar1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.PanelToolBar1.Location = New System.Drawing.Point(861, 0)
+        Me.PanelToolBar1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PanelToolBar1.Name = "PanelToolBar1"
+        Me.PanelToolBar1.Size = New System.Drawing.Size(145, 83)
+        Me.PanelToolBar1.TabIndex = 7
+        '
+        'ButtonX1
+        '
+        Me.ButtonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.ButtonX1.BackColor = System.Drawing.Color.Transparent
+        Me.ButtonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.Blue
+        Me.ButtonX1.DisabledImagesGrayScale = False
+        Me.ButtonX1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.ButtonX1.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonX1.Image = Global.TeVendo.My.Resources.Resources.printee
+        Me.ButtonX1.ImageFixedSize = New System.Drawing.Size(35, 35)
+        Me.ButtonX1.Location = New System.Drawing.Point(0, 0)
+        Me.ButtonX1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ButtonX1.Name = "ButtonX1"
+        Me.ButtonX1.Size = New System.Drawing.Size(145, 83)
+        Me.ButtonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.ButtonX1.TabIndex = 1
+        Me.ButtonX1.Text = "Generar"
+        Me.ButtonX1.TextColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(141, Byte), Integer))
+        '
+        'Tec_ReporteIngresosEgresosMensuales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 622)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Tec_ReporteIngresosEgresosMensuales"
+        Me.Text = "Tec_ReporteIngresosEgresosMensuales"
+        Me.Panel1.ResumeLayout(False)
+        Me.PanelSuperior.ResumeLayout(False)
+        Me.PanelLEft.ResumeLayout(False)
+        Me.Panel8.ResumeLayout(False)
+        Me.PanelDatos.ResumeLayout(False)
+        Me.PanelPrincipal.ResumeLayout(False)
+        Me.Panel10.ResumeLayout(False)
+        Me.Panel11.ResumeLayout(False)
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelButton.ResumeLayout(False)
+        Me.PanelButton.PerformLayout()
+        Me.PanelToolBar1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PanelSuperior As Panel
+    Friend WithEvents PanelLEft As Panel
+    Friend WithEvents Panel8 As Panel
+    Friend WithEvents PanelDatos As Panel
+    Protected WithEvents PanelPrincipal As Panel
+    Private WithEvents MReportViewer As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Panel10 As Panel
+    Friend WithEvents Panel11 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Panel12 As Panel
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PanelButton As Panel
+    Protected WithEvents PanelNavegacion As Panel
+    Protected WithEvents PanelToolBar1 As Panel
+    Protected WithEvents ButtonX1 As DevComponents.DotNetBar.ButtonX
+End Class
---
+++ TeVendo/Caja/Tec_ReporteIngresosEgresosMensuales.vb
@@ -0,0 +1,49 @@
+﻿Imports Negocio.AccesoLogica
+
+Imports DevComponents.DotNetBar
+Imports System.IO
+Imports DevComponents.DotNetBar.SuperGrid.Style
+Imports Janus.Data
+Public Class Tec_ReporteIngresosEgresosMensuales
+    Dim RutaGlobal As String = gs_CarpetaRaiz
+    Dim RutaTemporal As String = "C:\Temporal"
+    Public Sub _prIniciarTodo()
+
+        Me.Text = "REPORTE INGRESOS/EGRESOS MENSUALES"
+        MReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+
+
+    End Sub
+
+    Private Sub Tec_ReporteIngresosEgresosMensuales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _prIniciarTodo()
+    End Sub
+
+    Private Sub ButtonX1_Click(sender As Object, e As EventArgs) Handles ButtonX1.Click
+        Dim _dt As New DataTable
+        _dt = ReporteSaldosValorados()
+        If (IsNothing(_dt) Or _dt.Rows.Count = 0) Then
+
+            Dim img As Bitmap = New Bitmap(My.Resources.mensaje, 50, 50)
+            ToastNotification.Show(Me, "No Existen Datos Para Mostrar con Los Filtros Seleccionados".ToUpper, img, 5000, eToastGlowColor.Red, eToastPosition.TopCenter)
+
+            Return
+
+        End If
+
+
+        If (_dt.Rows.Count > 0) Then
+
+            Dim objrep As New R_SaldosValorados
+
+            objrep.SetDataSource(_dt)
+            objrep.SetParameterValue("Usuario", L_Usuario)
+            MReportViewer.ReportSource = objrep
+            MReportViewer.Show()
+            MReportViewer.BringToFront()
+        Else
+            Dim img As Bitmap = New Bitmap(My.Resources.mensaje, 50, 50)
+            ToastNotification.Show(Me, "No Existen Datos Para Mostrar. con Los Filtros Seleccionados".ToUpper, img, 5000, eToastGlowColor.Red, eToastPosition.TopCenter)
+        End If
+    End Sub
+End Class
