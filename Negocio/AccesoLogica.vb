3d27e26b44ac830398e9def99b97513dbb962df5
MArco Mamani <marcoantoniomamanichura@gmail.com>
2022-11-13 17:21:14 -0400

    reporte cierre de caja

--- Negocio/AccesoLogica.vb
+++ Negocio/AccesoLogica.vb
@@ -1505,6 +1505,18 @@ Public Class AccesoLogica
 
         Return _Tabla
     End Function
+    Public Shared Function L_prReporteCierreCajero(CierreId As Integer) As DataTable
+        Dim _Tabla As DataTable
+
+        Dim _listParam As New List(Of Datos.DParametro)
+
+        _listParam.Add(New Datos.DParametro("@tipo", 12))
+        _listParam.Add(New Datos.DParametro("@usuario", L_Usuario))
+        _listParam.Add(New Datos.DParametro("@Id", CierreId))
+        _Tabla = D_ProcedimientoConParam("MAM_CierreCajero", _listParam)
+
+        Return _Tabla
+    End Function
     Public Shared Function L_prListarProductosVentas(_deposito As Integer, _Precio As Integer) As DataTable
         Dim _Tabla As DataTable
 
