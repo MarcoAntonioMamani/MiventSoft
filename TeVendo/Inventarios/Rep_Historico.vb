5ba6752c41426dcf2d5b057ae6847d025590b5c1
MArco Mamani <marcoantoniomamanichura@gmail.com>
2023-10-13 06:48:01 -0400

    camnbios

--- TeVendo/Inventarios/Rep_Historico.vb
+++ TeVendo/Inventarios/Rep_Historico.vb
@@ -9,7 +9,7 @@ Public Class Rep_Historico
     Dim IdCliente As Integer = 0
 
     Public Sub _prIniciarTodo()
-        cbFechaDesde.Value = Now.Date
+        cbFechaDesde.Value = Now.Date.AddYears(-2)
         cbFechaHasta.Value = Now.Date
         tbVendedor.ReadOnly = True
         Me.Text = "REPORTE HISTORICO CLIENTES"
@@ -97,31 +97,54 @@ Public Class Rep_Historico
 
         Dim dtCredito As DataTable = L_EstructuraVentasContadoCredito(IdCliente, cbFechaDesde.Value.ToString("yyyy/MM/dd"), cbFechaHasta.Value.ToString("yyyy/MM/dd"), 0)
 
+        Dim TotalDeuda As Double = 0
+        Dim TotalPagado As Double = 0
         For i As Integer = 0 To dtCredito.Rows.Count - 1 Step 1
 
+            dtCredito.Rows(i).Item("Haber") = dtCredito.Rows(i).Item("Debe")
             dtContado.ImportRow(dtCredito.Rows(i))
-
 
             Dim dtpagos As DataTable = L_EstadoDeCuentasPorCobrarFechas(dtCredito.Rows(i).Item("creditoId"), cbFechaDesde.Value.ToString("yyyy/MM/dd"), cbFechaHasta.Value.ToString("yyyy/MM/dd"))
 
             Dim saldo As Double = dtCredito.Rows(i).Item("Debe")
+
+            TotalDeuda = TotalDeuda + dtCredito.Rows(i).Item("Debe")
+
+
             For j As Integer = 0 To dtpagos.Rows.Count - 1 Step 1
 
                 saldo = saldo - dtpagos.Rows(j).Item("Monto")
-                dtContado.Rows.Add(dtpagos.Rows(j).Item("FechaPago"),
+                dtContado.Rows.Add(dtpagos.Rows(j).Item("FechaPago"), dtpagos.Rows(j).Item("FechaPago"),
                                        dtCredito.Rows(i).Item("Cliente"), dtCredito.Rows(i).Item("CodigoTransaccion"),
-                                       "Registro De Pago - Recibo# " + dtpagos.Rows(j).Item("NroRecibo"), 0, dtpagos.Rows(j).Item("Monto"), saldo)
+                                       "Registro De Pago De Credito - Recibo# " + dtpagos.Rows(j).Item("NroRecibo") + " - Del Recibo Venta Manual # " + dtpagos.Rows(j).Item("ReciboManual"), dtpagos.Rows(j).Item("Monto"), dtpagos.Rows(j).Item("Monto"), saldo)
+
+                TotalPagado = TotalPagado + dtpagos.Rows(j).Item("Monto")
             Next
 
         Next
-
+        For i As Integer = 0 To dtContado.Rows.Count - 1 Step 1
+            dtContado.Rows(i).Item("TotalDeuda") = TotalDeuda - TotalPagado
+        Next
         If (dtContado.Rows.Count > 0) Then
 
-            Dim objrep As New ExtractoCliente
-            objrep.SetDataSource(dtContado)
+
+            dtContado.DefaultView.Sort = "fecha ASC"
+
+            ' Ahora puedes acceder a los datos ordenados utilizando DefaultView
+            Dim datosOrdenados As DataView = dtContado.DefaultView
+
+            ' Si deseas asignar los datos ordenados de nuevo a un DataTable
+            Dim miTablaOrdenada As New DataTable()
+            miTablaOrdenada = datosOrdenados.ToTable()
+
+
+            Dim objrep As New HistoricoCliente
+            objrep.SetDataSource(miTablaOrdenada)
             Dim fechaI As String = cbFechaDesde.Value.ToString("dd/MM/yyyy")
             Dim fechaF As String = cbFechaHasta.Value.ToString("dd/MM/yyyy")
             objrep.SetParameterValue("Usuario", L_Usuario)
+            objrep.SetParameterValue("Desde", fechaI)
+            objrep.SetParameterValue("Hasta", fechaF)
             MReportViewer.ReportSource = objrep
             MReportViewer.Show()
             MReportViewer.BringToFront()
