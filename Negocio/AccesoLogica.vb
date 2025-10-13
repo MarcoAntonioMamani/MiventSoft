c464ed0e0aa8b43c3d2afc491d139d0f8748e5a6
MArco <marcoantoniomamanichura@gmail.com>
2025-10-13 19:19:16 -0400

    cambios

--- Negocio/AccesoLogica.vb
+++ Negocio/AccesoLogica.vb
@@ -1433,6 +1433,19 @@ Public Class AccesoLogica
 
         Return _Tabla
     End Function
+
+    Public Shared Function pr_ObtenerDeuda(ClienteId As String) As DataTable
+        Dim _Tabla As DataTable
+
+        Dim _listParam As New List(Of Datos.DParametro)
+
+        _listParam.Add(New Datos.DParametro("@tipo", 13))
+        _listParam.Add(New Datos.DParametro("@usuario", L_Usuario))
+        _listParam.Add(New Datos.DParametro("@ClienteId", ClienteId))
+        _Tabla = D_ProcedimientoConParam("MAM_Ventas", _listParam)
+
+        Return _Tabla
+    End Function
     Public Shared Function ListarVentaRecibo(VentaId As String) As DataTable
         Dim _Tabla As DataTable
 
